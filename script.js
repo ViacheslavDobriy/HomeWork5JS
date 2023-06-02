bed5c8fdd64b21a5ba92f4bd91e8061cf7140c35
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-06-02 04:01:37 +0100

    HomeWork is done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Task 1
+// TASK 1
 
 const numbers = {
     keyin1: 1,
@@ -20,11 +20,41 @@ function findThreeAndMore (object) {
 
 findThreeAndMore(numbers);
 
-// Task 2
+// TASK 2
+
+const post = {
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2, // вывести это число
+            },
+        },
+        {
+            userId: 5, // вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", // вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1,
+            },
+        },
+    ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
 
 // TASk 3
 
-const products = [
+const products3 = [
     {
         id: 3,
         price: 200
@@ -34,13 +64,105 @@ const products = [
         price: 900
     },
     {
-        id:1,
+        id: 1,
         price: 1000
     }
 ];
 
-function makeExpensive(array, extraCharge) {
+function applyDiscount(array, discount) {
     array.forEach(element => {
-        
+        for (const key in element) {
+            if (key == "price") {
+                element[key] *= 1 - discount;
+            }
+        }
     });
 }
+console.log(products3);
+applyDiscount(products3, 0.15);
+console.log(products3);
+
+// TASK 4
+
+const products4 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+/**
+ * Булевая функция, которая проверяет наличие свойства photos в объекте,
+ *  а также наличие хотя бы одного элемента в этом свойстве
+ * @param {*} object - принимаемый объект products 
+ * @returns возвращает true если условие удовлетворено,
+ *  и false если не было найдено свойство photos или если это свойство пустое
+ */
+function doHavePhotos(object) {
+    for (const key in object) {
+        if (key == 'photos' && object[key].length > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const filteredProductsByImage = products4.filter(doHavePhotos);
+console.log(filteredProductsByImage);
+
+/**
+ * Функция сравнивает два объекта исключительно по свойству price
+ * @param {*} objectA 1ый объект
+ * @param {*} objectB 2ой объект
+ * @returns возвращает 1 или -1 в зависимости от необходимой сортировки,
+ *  в данном случае представлена сортировка по возрастанию цены
+ */
+function comparePrices(objectA, objectB) {
+    for (const key in objectA) {
+        if (key == 'price') {
+            if (objectA[key] > objectB[key]) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+}
+
+const sortedProductsByPrice = products4.sort(comparePrices);
+console.log(sortedProductsByPrice);
+
+// TASK 5
+
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+function arraysToObject(arr1, arr2) {
+    const resultObject = {};
+    for (let i = 0; i < arr1.length; i++) {
+        resultObject[arr1[i]] = arr2[i];
+    }
+    return resultObject;
+}
+
+console.log(arraysToObject(en, ru));
